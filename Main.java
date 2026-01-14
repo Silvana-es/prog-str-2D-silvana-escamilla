f707761c78b3930ea6c817690238c66154d60439
Silvana Escamilla <20253DS102@utez.edu.mx>
2026-01-13 22:16:30 -0600

    Cambios locales antes del pull

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void Main(String[] args) {
-        System.out.println("Nombre: Silvana Lizeth Escamilla Carrillo");
-        System.out.println("Grupo: 2D");
-        System.out.println("Fecha: 17 de Julio del 2007");
-        System.out.println("Hola mundo");
-    }
-}
