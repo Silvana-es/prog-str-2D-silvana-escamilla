028c572cd4e077e408404363248407f97aad694e
Silvana Escamilla <20253DS102@utez.edu.mx>
2026-01-17 21:11:52 -0600

    Se crea rama dev y se edita .gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Nombre: Silvana Lizeth Escamilla Carrillo");
         System.out.println("Grupo: 2D");
         System.out.println("Fecha: 17 de Julio del 2007");
