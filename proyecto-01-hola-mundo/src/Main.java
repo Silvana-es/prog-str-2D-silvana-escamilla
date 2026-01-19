a9faac63a606f1d088aa8d2de5942771b3740758
Silvana-es <20253ds102.edu.mx>
2026-01-18 20:24:26 -0600

    Proyecto 01: hola mundo

---
+++ proyecto-01-hola-mundo/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Silvana Lizeth Escamilla Carrillo");
+        System.out.println("Grupo: 2D");
+        System.out.println("Fecha: 17 de Julio del 2007");
+        System.out.println("Hola mundo");
+    }
+}
